74e66d798215d3070012e1c713ebe10a7cbbb930
Stepkos <stepkos@wp.pl>
2020-04-04 18:35:40 +0200

    words_manager:display_topics

--- words_manager/views.py
+++ words_manager/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import *
+
+def display_topics(request):
+    topics = Topic.objects.order_by('-date_added')
+    return render(request, 'words_manager/display_topics.html', {'topics': topics})
+
+def add_topic(request):
+    pass
