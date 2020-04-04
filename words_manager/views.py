116cd098801b04421a3cbb7f15c01d6de52ded1f
Stepkos <stepkos@wp.pl>
2020-04-04 19:02:12 +0200

    Add new topic

--- words_manager/views.py
+++ words_manager/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+# Functions import
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
-# Create your views here.
+# Objects import
 from .models import *
+
+# Forms import
+from .forms import TopicForm
+
+# Create your views here
 
 def display_topics(request):
     topics = Topic.objects.order_by('-date_added')
     return render(request, 'words_manager/display_topics.html', {'topics': topics})
 
 def add_topic(request):
-    pass
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('words_manager:display_topics'))
+            
+    return render(request, 'words_manager/add_topic.html', {'form': form})
