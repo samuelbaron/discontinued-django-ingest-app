23a905178d874cca7b44d91a960cf716919269ac
stepkos <52202262+stepkos@users.noreply.github.com>
2020-04-06 15:19:30 +0200

    stepkos
    This is branch to project additives

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\Dokumenty\obrobka\Programowanie Webowe\Projects\Projects\ingest_app\Master\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
