de5fed34d75ebb7e4cc31af8b9e3ee43f50bb9fe
samuelbaron <samuel.baron.pl@gmail.com>
2020-04-07 14:36:16 +0200

    Revert "words_manager:display_topics"
    This reverts commit 74e66d798215d3070012e1c713ebe10a7cbbb930.

--- words_manager/urls.py
+++ words_manager/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 app_name = 'words_manager'
 urlpatterns = [
-    path('display_topics/', views.display_topics, name='display_topics')
-    #path('add_topics/', views.add_topics, name='add_topics')
+    #path('', views.topics, name='topics')
 ]
