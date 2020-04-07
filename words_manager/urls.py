6a76d092b41f73e2f37710faabe97e7bcbf78b92
stepkos <52202262+stepkos@users.noreply.github.com>
2020-04-07 13:50:37 +0200

    New URLs

--- words_manager/urls.py
+++ words_manager/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 app_name = 'words_manager'
 urlpatterns = [
-    path('sets_display/', views.sets_display, name='sets_display'),
-    path('set_add/', views.set_add, name='set_add'),
+    path('all/', views.sets_display, name='sets_display'),
+    path('add/', views.set_add, name='set_add'),
+
+    #path('<int:set_id>/', views.set_display, name='set_display'),
+    #path('<int:set_id>/edit/', views.set_edit, name='set_edit'),
+    #path('<int:set_id>/add/', views.word_add, name='word_add'),
+    #path('<int:set_id>/<int:word_id>/edit/', views.word_edit, name='word_edit'),
 ]
