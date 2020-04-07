4306835b085cf196fa2fc72cb7f726e8f2c1d272
samuelbaron <samuel.baron.pl@gmail.com>
2020-04-07 15:10:43 +0200

    new branch

---
+++ javascript/index.js
@@ -0,0 +1,105 @@
+function open_home_content() {
+    document.getElementById("about_content").style.display = "none";
+    document.getElementById("contact_content").style.display = "none";
+    document.getElementById("home_content").style.display = "block";
+}
+
+function open_about_content() {
+    document.getElementById("home_content").style.display = "none";
+    document.getElementById("contact_content").style.display = "none";
+    document.getElementById("about_content").style.display = "block";
+}
+
+function open_contact_content() {
+    document.getElementById("home_content").style.display = "none";
+    document.getElementById("about_content").style.display = "none";
+    document.getElementById("contact_content").style.display = "block";
+}
+
+function open_login_content() {
+    document.getElementById("home_content").style.display = "none";
+    document.getElementById("login_content").style.display = "block";
+}
+
+function open_home_content_from_login_content() {
+    document.getElementById("login_content").style.display = "none";
+    document.getElementById("about_content").style.display = "none";
+    document.getElementById("contact_content").style.display = "none";
+    document.getElementById("home_content").style.display = "block";
+}
+
+function open_about_content_from_login_content() {
+    document.getElementById("login_content").style.display = "none";
+    document.getElementById("home_content").style.display = "none";
+    document.getElementById("contact_content").style.display = "none";
+    document.getElementById("about_content").style.display = "block";
+}
+
+function open_contact_content_from_login_content() {
+    document.getElementById("login_content").style.display = "none";
+    document.getElementById("home_content").style.display = "none";
+    document.getElementById("about_content").style.display = "none";
+    document.getElementById("contact_content").style.display = "block";
+}
+
+function open_sign_in_content() {
+    document.getElementById("login_content").style.display = "none";
+    document.getElementById("sign_in_content").style.display = "block";
+}
+
+function open_sign_up_content() {
+    document.getElementById("login_content").style.display = "none";
+    document.getElementById("sign_up_content").style.display = "block";
+}
+
+function open_home_content_from_sign_in_content() {
+    document.getElementById("sign_in_content").style.display = "none";
+    document.getElementById("about_content").style.display = "none";
+    document.getElementById("contact_content").style.display = "none";
+    document.getElementById("home_content").style.display = "block";
+}
+
+function open_about_content_from_sign_in_content() {
+    document.getElementById("sign_in_content").style.display = "none";
+    document.getElementById("home_content").style.display = "none";
+    document.getElementById("contact_content").style.display = "none";
+    document.getElementById("about_content").style.display = "block";
+}
+
+function open_contact_content_from_sign_in_content() {
+    document.getElementById("sign_in_content").style.display = "none";
+    document.getElementById("home_content").style.display = "none";
+    document.getElementById("about_content").style.display = "none";
+    document.getElementById("contact_content").style.display = "block";
+}
+
+function open_sign_up_content_from_sign_in() {
+    document.getElementById("sign_in_content").style.display = "none";
+    document.getElementById("sign_up_content").style.display = "block";
+}
+
+function open_sign_in_content_from_sign_up() {
+    document.getElementById("sign_up_content").style.display = "none";
+    document.getElementById("sign_in_content").style.display = "block";
+}
+
+function open_home_content_from_sign_up_content() {
+    document.getElementById("sign_up_content").style.display = "none";
+    document.getElementById("about_content").style.display = "none";
+    document.getElementById("contact_content").style.display = "none";
+    document.getElementById("home_content").style.display = "block";
+}
+
+function open_about_content_from_sign_up_content() {
+    document.getElementById("sign_up_content").style.display = "none";
+    document.getElementById("home_content").style.display = "none";
+    document.getElementById("contact_content").style.display = "none";
+    document.getElementById("about_content").style.display = "block";
+}
+
+function open_contact_content_from_sign_up_content() {
+    document.getElementById("sign_up_content").style.display = "none";
+    document.getElementById("home_content").style.display = "none";
+    document.getElementById("about_content").style.display = "none";
+    document.getElementById("contact_content").style.display = "block";
+}
