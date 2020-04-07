ab9f1380630ce9ccf518cebdab6c21f82beb50c4
samuelbaron <samuel.baron.pl@gmail.com>
2020-04-07 14:36:15 +0200

    Revert "Add new topic"
    This reverts commit 116cd098801b04421a3cbb7f15c01d6de52ded1f.

--- words_manager/urls.py
+++ words_manager/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'words_manager'
 urlpatterns = [
-    path('', views.display_topics, name='display_topics'),
-    path('add_topic/', views.add_topic, name='add_topic'),
+    path('display_topics/', views.display_topics, name='display_topics')
+    #path('add_topics/', views.add_topics, name='add_topics')
 ]
--- words_manager/views.py
+++ words_manager/views.py
@@ -1,26 +1,11 @@
-# Functions import
-from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.shortcuts import render
 
-# Objects import
+# Create your views here.
 from .models import *
-
-# Forms import
-from .forms import TopicForm
-
-# Create your views here
 
 def display_topics(request):
     topics = Topic.objects.order_by('-date_added')
     return render(request, 'words_manager/display_topics.html', {'topics': topics})
 
 def add_topic(request):
-    if request.method != 'POST':
-        form = TopicForm()
-    else:
-        form = TopicForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect(reverse('words_manager:display_topics'))
-            
-    return render(request, 'words_manager/add_topic.html', {'form': form})
+    pass
