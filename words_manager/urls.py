40b1cb848c8a328cca020077cb38a28ec6d9fbdb
stepkos <52202262+stepkos@users.noreply.github.com>
2020-04-08 14:15:57 +0200

    Set display and 404 error

--- words_manager/urls.py
+++ words_manager/urls.py
@@ -6,8 +6,8 @@ app_name = 'words_manager'
 urlpatterns = [
     path('all/', views.sets_display, name='sets_display'),
     path('add/', views.set_add, name='set_add'),
+    path('<int:set_id>/', views.set_display, name='set_display'),
 
-    #path('<int:set_id>/', views.set_display, name='set_display'),
     #path('<int:set_id>/edit/', views.set_edit, name='set_edit'),
     #path('<int:set_id>/add/', views.word_add, name='word_add'),
     #path('<int:set_id>/<int:word_id>/edit/', views.word_edit, name='word_edit'),
--- words_manager/views.py
+++ words_manager/views.py
@@ -1,6 +1,7 @@
 # Functions import
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+#from django.http import Http404
 
 # Objects import
 from .models import *
@@ -24,3 +25,18 @@ def set_add(request):
             return redirect(reverse('words_manager:sets_display'))
 
     return render(request, 'words_manager/set_add.html', {'form': form})
+
+
+def set_display(request, set_id):
+    '''try:
+        spec_set = Set.objects.get(id=set_id)
+    except Set.DoesNotExist:
+        raise Http404('Erro 404 XDDDD')'''
+
+    spec_set = get_object_or_404(Set, pk=set_id)
+
+    words = spec_set.word_set.all()
+    context = {'set': spec_set, 'words': words}
+    return render(request, 'words_manager/set_display.html', context)
+
+    
