a40522d05f57f174af62c8f9e4884c9e868f81fb
samuelbaron <samuel.baron.pl@gmail.com>
2020-04-08 13:20:41 +0200

    Added first part of password_resseter.html

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,4 @@
+//INDEX NAVIGATION SYSTEM
 function open_home_content() {
     document.getElementById("about_content").style.display = "none";
     document.getElementById("contact_content").style.display = "none";
@@ -20,7 +21,6 @@ function open_login_content() {
     document.getElementById("home_content").style.display = "none";
     document.getElementById("login_content").style.display = "block";
 }
-
 function open_home_content_from_login_content() {
     document.getElementById("login_content").style.display = "none";
     document.getElementById("about_content").style.display = "none";
@@ -47,11 +47,6 @@ function open_sign_in_content() {
     document.getElementById("sign_in_content").style.display = "block";
 }
 
-function open_sign_up_content() {
-    document.getElementById("login_content").style.display = "none";
-    document.getElementById("sign_up_content").style.display = "block";
-}
-
 function open_home_content_from_sign_in_content() {
     document.getElementById("sign_in_content").style.display = "none";
     document.getElementById("about_content").style.display = "none";
@@ -71,6 +66,11 @@ function open_contact_content_from_sign_in_content() {
     document.getElementById("home_content").style.display = "none";
     document.getElementById("about_content").style.display = "none";
     document.getElementById("contact_content").style.display = "block";
+}
+
+function open_sign_up_content() {
+    document.getElementById("login_content").style.display = "none";
+    document.getElementById("sign_up_content").style.display = "block";
 }
 
 function open_sign_up_content_from_sign_in() {
@@ -103,3 +103,4 @@ function open_contact_content_from_sign_up_content() {
     document.getElementById("about_content").style.display = "none";
     document.getElementById("contact_content").style.display = "block";
 }
+
